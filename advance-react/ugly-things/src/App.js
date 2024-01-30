60fcd4465f812335b25bb38b3f81f3c9a438c431
Takeshi Garcia <garcia.takeshi@yahoo.com>
2024-01-29 16:50:22 -0800

    update

---
+++ advance-react/ugly-things/src/App.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import uglyThingsData from './UglyThingsData'
+import Header from './Header'
+import UglyCard from './UglyCard'
+
+
+function App() {
+
+  const [uglyData, setUglyData] = useState([])
+
+    // GET REQUEST
+  useEffect(() => {
+    axios.get("https://api.vschool.io/takeshigarcia/thing")
+      .then(res => setUglyData(res.data))
+  }, [])
+
+    // INPUT 
+ const [inputData, setInputData] = useState({
+  title: "",
+  imgUrl: "",
+  description: ""
+ })
+
+ function InputListener(e){
+  const {name, value} = e.target
+  setInputData(prevData => ({
+    ...prevData,
+    [name]: value
+  }))
+ }
+
+ 
+      // SUBMIT FORM
+      function submitUglyData() {
+        axios.post("https://api.vschool.io/takeshigarcia/thing", inputData)
+          .then(res => {
+            setUglyData(prevData => ([
+              ...prevData,
+              res.data
+            ]))
+            setInputData({
+              title: "",
+              imgUrl: "",
+              description: ""
+            });
+          })
+          .catch(error => {
+            // Handle error if needed
+            console.error("Error submitting ugly data:", error);
+          });
+      }
+
+  // DELETE BUTTON
+  function deleteUgly(id){
+    axios.delete(`https://api.vschool.io/takeshigarcia/thing/${id}`)
+      .then(setUglyData(prevUglyData => {
+        return prevUglyData.filter(card => card._id !== id)
+      }))
+  }
+
+
+  // EDIT UPDATE BUTTON
+  function editUgly(id, editField){
+    axios.put(`https://api.vschool.io/takeshigarcia/thing/${id}`, editField)
+      .then(setUglyData(prevUglyData => {
+          return prevUglyData.map(card => {
+            if(card._id === id){
+              return{
+                ...card,
+                ...editField
+              }
+            }
+            return card
+          })
+        })
+      )
+  }
+
+  const card = uglyData.map(data => (
+    <UglyCard 
+      data={data}
+      key={data._id}
+    />
+  ))
+ 
+  return (
+    <uglyThingsData.Provider value={{
+      uglyData,
+      inputData,
+      InputListener,
+      submitUglyData,
+      deleteUgly,
+      editUgly
+    }}>
+      <div>
+        <Header />
+        {card}
+      </div>
+    </uglyThingsData.Provider>
+  )
+}
+
+export default App
